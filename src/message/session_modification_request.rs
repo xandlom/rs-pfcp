9acfdabb05da4a3bb713539c6a93661044207076
Andrei Lomov <xandlom@gmail.com>
2025-08-18 14:59:55 +0200

    feat: Update SessionModificationRequest with all IEs
    Updated the SessionModificationRequest to include all mandatory and optional Information Elements, aligning it with the SessionEstablishmentRequest for better consistency and completeness.

--- src/message/session_modification_request.rs
+++ src/message/session_modification_request.rs
@@ -27,6 +27,15 @@ pub struct SessionModificationRequest {
     pub update_qers: Option<Vec<Ie>>,
     pub update_bars: Option<Vec<Ie>>,
     pub update_traffic_endpoints: Option<Vec<Ie>>,
+    pub pdn_type: Option<Ie>,
+    pub user_id: Option<Ie>,
+    pub s_nssai: Option<Ie>,
+    pub trace_information: Option<Ie>,
+    pub recovery_time_stamp: Option<Ie>,
+    pub cp_function_features: Option<Ie>,
+    pub apn_dnn: Option<Ie>,
+    pub user_plane_inactivity_timer: Option<Ie>,
+    pub pfcpsm_req_flags: Option<Ie>,
     pub ies: Vec<Ie>,
 }
 
@@ -126,6 +135,33 @@ impl Message for SessionModificationRequest {
                 data.extend_from_slice(&ie.marshal());
             }
         }
+        if let Some(ie) = &self.pdn_type {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.user_id {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.s_nssai {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.trace_information {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.recovery_time_stamp {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.cp_function_features {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.apn_dnn {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.user_plane_inactivity_timer {
+            data.extend_from_slice(&ie.marshal());
+        }
+        if let Some(ie) = &self.pfcpsm_req_flags {
+            data.extend_from_slice(&ie.marshal());
+        }
         for ie in &self.ies {
             data.extend_from_slice(&ie.marshal());
         }
@@ -153,6 +189,15 @@ impl Message for SessionModificationRequest {
         let mut update_qers = None;
         let mut update_bars = None;
         let mut update_traffic_endpoints = None;
+        let mut pdn_type = None;
+        let mut user_id = None;
+        let mut s_nssai = None;
+        let mut trace_information = None;
+        let mut recovery_time_stamp = None;
+        let mut cp_function_features = None;
+        let mut apn_dnn = None;
+        let mut user_plane_inactivity_timer = None;
+        let mut pfcpsm_req_flags = None;
         let mut ies = Vec::new();
 
         let mut offset = header.len() as usize;
@@ -185,6 +230,15 @@ impl Message for SessionModificationRequest {
                 IeType::UpdateTrafficEndpoint => {
                     update_traffic_endpoints.get_or_insert(Vec::new()).push(ie)
                 }
+                IeType::PdnType => pdn_type = Some(ie),
+                IeType::UserId => user_id = Some(ie),
+                IeType::Snssai => s_nssai = Some(ie),
+                IeType::TraceInformation => trace_information = Some(ie),
+                IeType::RecoveryTimeStamp => recovery_time_stamp = Some(ie),
+                IeType::CpFunctionFeatures => cp_function_features = Some(ie),
+                IeType::ApnDnn => apn_dnn = Some(ie),
+                IeType::UserPlaneInactivityTimer => user_plane_inactivity_timer = Some(ie),
+                IeType::PfcpsmReqFlags => pfcpsm_req_flags = Some(ie),
                 _ => ies.push(ie),
             }
             offset += ie_len;
@@ -211,6 +265,15 @@ impl Message for SessionModificationRequest {
             update_qers,
             update_bars,
             update_traffic_endpoints,
+            pdn_type,
+            user_id,
+            s_nssai,
+            trace_information,
+            recovery_time_stamp,
+            cp_function_features,
+            apn_dnn,
+            user_plane_inactivity_timer,
+            pfcpsm_req_flags,
             ies,
         })
     }
@@ -236,12 +299,19 @@ impl Message for SessionModificationRequest {
     }
 
     fn find_ie(&self, ie_type: IeType) -> Option<&Ie> {
-        if let Some(ie) = &self.fseid {
-            if ie.ie_type == ie_type {
-                return Some(ie);
-            }
+        match ie_type {
+            IeType::Fseid => self.fseid.as_ref(),
+            IeType::PdnType => self.pdn_type.as_ref(),
+            IeType::UserId => self.user_id.as_ref(),
+            IeType::Snssai => self.s_nssai.as_ref(),
+            IeType::TraceInformation => self.trace_information.as_ref(),
+            IeType::RecoveryTimeStamp => self.recovery_time_stamp.as_ref(),
+            IeType::CpFunctionFeatures => self.cp_function_features.as_ref(),
+            IeType::ApnDnn => self.apn_dnn.as_ref(),
+            IeType::UserPlaneInactivityTimer => self.user_plane_inactivity_timer.as_ref(),
+            IeType::PfcpsmReqFlags => self.pfcpsm_req_flags.as_ref(),
+            _ => self.ies.iter().find(|ie| ie.ie_type == ie_type),
         }
-        self.ies.iter().find(|ie| ie.ie_type == ie_type)
     }
 }
 
@@ -267,6 +337,15 @@ pub struct SessionModificationRequestBuilder {
     update_qers: Option<Vec<Ie>>,
     update_bars: Option<Vec<Ie>>,
     update_traffic_endpoints: Option<Vec<Ie>>,
+    pdn_type: Option<Ie>,
+    user_id: Option<Ie>,
+    s_nssai: Option<Ie>,
+    trace_information: Option<Ie>,
+    recovery_time_stamp: Option<Ie>,
+    cp_function_features: Option<Ie>,
+    apn_dnn: Option<Ie>,
+    user_plane_inactivity_timer: Option<Ie>,
+    pfcpsm_req_flags: Option<Ie>,
     ies: Vec<Ie>,
 }
 
@@ -294,6 +373,15 @@ impl SessionModificationRequestBuilder {
             update_qers: None,
             update_bars: None,
             update_traffic_endpoints: None,
+            pdn_type: None,
+            user_id: None,
+            s_nssai: None,
+            trace_information: None,
+            recovery_time_stamp: None,
+            cp_function_features: None,
+            apn_dnn: None,
+            user_plane_inactivity_timer: None,
+            pfcpsm_req_flags: None,
             ies: Vec::new(),
         }
     }
@@ -390,6 +478,51 @@ impl SessionModificationRequestBuilder {
 
     pub fn update_traffic_endpoints(mut self, update_traffic_endpoints: Vec<Ie>) -> Self {
         self.update_traffic_endpoints = Some(update_traffic_endpoints);
+        self
+    }
+
+    pub fn pdn_type(mut self, pdn_type: Ie) -> Self {
+        self.pdn_type = Some(pdn_type);
+        self
+    }
+
+    pub fn user_id(mut self, user_id: Ie) -> Self {
+        self.user_id = Some(user_id);
+        self
+    }
+
+    pub fn s_nssai(mut self, s_nssai: Ie) -> Self {
+        self.s_nssai = Some(s_nssai);
+        self
+    }
+
+    pub fn trace_information(mut self, trace_information: Ie) -> Self {
+        self.trace_information = Some(trace_information);
+        self
+    }
+
+    pub fn recovery_time_stamp(mut self, recovery_time_stamp: Ie) -> Self {
+        self.recovery_time_stamp = Some(recovery_time_stamp);
+        self
+    }
+
+    pub fn cp_function_features(mut self, cp_function_features: Ie) -> Self {
+        self.cp_function_features = Some(cp_function_features);
+        self
+    }
+
+    pub fn apn_dnn(mut self, apn_dnn: Ie) -> Self {
+        self.apn_dnn = Some(apn_dnn);
+        self
+    }
+
+    pub fn user_plane_inactivity_timer(mut self, user_plane_inactivity_timer: Ie) -> Self {
+        self.user_plane_inactivity_timer = Some(user_plane_inactivity_timer);
+        self
+    }
+
+    pub fn pfcpsm_req_flags(mut self, pfcpsm_req_flags: Ie) -> Self {
+        self.pfcpsm_req_flags = Some(pfcpsm_req_flags);
         self
     }
 
@@ -493,6 +626,33 @@ impl SessionModificationRequestBuilder {
                 payload_len += ie.len();
             }
         }
+        if let Some(ie) = &self.pdn_type {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.user_id {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.s_nssai {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.trace_information {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.recovery_time_stamp {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.cp_function_features {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.apn_dnn {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.user_plane_inactivity_timer {
+            payload_len += ie.len();
+        }
+        if let Some(ie) = &self.pfcpsm_req_flags {
+            payload_len += ie.len();
+        }
         for ie in &self.ies {
             payload_len += ie.len();
         }
@@ -524,6 +684,15 @@ impl SessionModificationRequestBuilder {
             update_qers: self.update_qers,
             update_bars: self.update_bars,
             update_traffic_endpoints: self.update_traffic_endpoints,
+            pdn_type: self.pdn_type,
+            user_id: self.user_id,
+            s_nssai: self.s_nssai,
+            trace_information: self.trace_information,
+            recovery_time_stamp: self.recovery_time_stamp,
+            cp_function_features: self.cp_function_features,
+            apn_dnn: self.apn_dnn,
+            user_plane_inactivity_timer: self.user_plane_inactivity_timer,
+            pfcpsm_req_flags: self.pfcpsm_req_flags,
             ies: self.ies,
         }
     }
